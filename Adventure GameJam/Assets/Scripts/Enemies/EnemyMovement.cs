515a22b617ae222d2e6b9b87dddcbc803083cf86
RebelRob32 <r.gue89@protonmail.com>
2023-05-28 14:16:40 -0500

    Enemy Moves
    Enemy wanders now

---
+++ Adventure GameJam/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AGJ.Enemy
+{
+public class EnemyMovement : MonoBehaviour
+{
+        public EnemyVariables stats;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private Vector3 walkPoint;
+        [SerializeField] private bool walkPointSet;
+        [SerializeField] NavMeshAgent agent;
+
+        public void Update()
+        {
+            Wander();
+        }
+
+
+        public void Wander()
+        {
+            if (!walkPointSet)
+            {
+                float randomZ = Random.Range(-stats.wanderRadius, stats.wanderRadius);
+                float randomX = Random.Range(-stats.wanderRadius, stats.wanderRadius);
+
+                walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+                if (Physics.Raycast(walkPoint, -transform.up, 2f, groundLayer))
+                {
+                    walkPointSet = true;
+                    //anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
+                }
+            }
+
+            if (walkPointSet == true)
+            {
+                agent.speed = stats.speed;
+                agent.SetDestination(walkPoint);
+                //anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+            }
+
+            Vector3 distToWalkPoint = transform.position - walkPoint;
+            if (distToWalkPoint.magnitude < 1f)
+            {
+                walkPointSet = false;
+            }
+
+
+        }
+
+    }
+
+
+}
+
