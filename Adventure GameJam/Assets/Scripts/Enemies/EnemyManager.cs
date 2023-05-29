1b100099a2dd02a21cb44ba4bd6d5f2c12271e17
RebelRob32 <r.gue89@protonmail.com>
2023-05-28 19:36:14 -0500

    Updates
    stuff added, stuff fixed, just tinkering

---
+++ Adventure GameJam/Assets/Scripts/Enemies/EnemyManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AGJ.Enemy
+{
+public class EnemyManager : MonoBehaviour
+{
+        public EnemyVariables stats;
+        public LayerMask playerLayer;
+        public bool playerFound;
+
+        public void DetectPlayer()
+        {
+
+        }
+}
+
+
+}
+
+
